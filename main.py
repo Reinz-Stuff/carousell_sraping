64afba036cec9d25c4a6d53eefb37279cc6a3cf1
Fauzi kurniawan <ozilpym123@gmail.com>
2022-11-30 08:18:37 +0700

    fix scraping scrool down

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
-from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-driver.get("https://id.carousell.com/categories/photography-6")
-driver.quit()
+driver.get('https://id.carousell.com/categories/photography-6')
+driver.maximize_window()
 
-# soup = BeautifulSoup(driver, 'html.parser')
-# page = soup.find(attrs='D_gc M_KN D_aHA')
-# content = page.find_all(attrs='D_iJ')
-# title = soup.find_all(attrs='D_jj M_he D_gS M_fi D_jk M_hf D_jn M_hi D_jp M_hk D_jt M_ho D_jw M_hr D_jg')
-# title.pop(0 - 1)
-# image = soup.find_all(attrs='D_uc D_ue')
-# print(soup)
-#
-# # for i in title:
-# #     print(i.text)
+bottom = False
+a = 0
+while not bottom:
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    driver.execute_script(f"window.scrollTo(0, {a});")
+    if a > new_height:
+        bottom = True
+    a += 10
+
+photography = []
+data = driver.find_elements(By.CSS_SELECTOR, 'main > div > div > div > div > div.D_os')
+for i in data:
+    judul = i.find_element(By.CSS_SELECTOR, 'p.D_jj.D_gU.D_jk.D_jn.D_jq.D_jt.D_jv.D_jr.D_jg').text
+    image = i.find_element(By.CSS_SELECTOR, 'a:nth-child(2) > div.D_o_ > div > div > img').get_dom_attribute('src')
+    toko = i.find_element(By.CSS_SELECTOR, 'a.D_ow.D_iJ > div.D_oz > p').text
+    harga = i.find_element(By.CSS_SELECTOR, 'a:nth-child(2) > div.D_oK > p').text
+    kondisi = i.find_element(By.CSS_SELECTOR, 'a:nth-child(2) > p:nth-child(4)').text
+
+    print(judul, harga, image)
+
+    list_barang = {
+        'judul': judul,
+        'image': image,
+        'toko': toko,
+        'harga': harga,
+        'kondisi': kondisi
+    }
+    photography.append(list_barang)
+
+print(photography)
+
+driver.quit()
