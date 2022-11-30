172a32b4451b5c1d5ffdceb7c6027434b7355266
Fauzi kurniawan <ozilpym123@gmail.com>
2022-11-30 08:14:25 +0700

    fix scraping scrool down

--- main2.py
+++ main2.py
@@ -1,29 +1,39 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
-import time
 
 driver = webdriver.Chrome()
 driver.get('https://id.carousell.com/categories/photography-6')
-WebDriverWait(driver, 20)
+driver.maximize_window()
 
-# while True:
-#     try:
-#         WebDriverWait(driver, 5).until(ec.presence_of_element_located((By.CSS_SELECTOR, '#main > div.D_aHi > div > div.D_aHu > main > div > button'))).click()
-#         time.sleep(2)
-#     except Exception as e:
-#         print(e)
-#         break
+bottom = False
+a = 0
+while not bottom:
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    driver.execute_script(f"window.scrollTo(0, {a});")
+    if a > new_height:
+        bottom = True
+    a += 10
 
 photography = []
 data = driver.find_elements(By.CSS_SELECTOR, 'main > div > div > div > div > div.D_os')
 for i in data:
-    judul = i.find_element(By.CSS_SELECTOR, 'p.D_jj.D_gU.D_jk.D_jn.D_jq.D_jt.D_jv.D_jr.D_jg')
-    image = i.find_element(By.CSS_SELECTOR, 'main > div > div > div > div > div.D_os > a:nth-child(2) > div.D_o_ > div > div > img').get_dom_attribute('src')
+    judul = i.find_element(By.CSS_SELECTOR, 'p.D_jj.D_gU.D_jk.D_jn.D_jq.D_jt.D_jv.D_jr.D_jg').text
+    image = i.find_element(By.CSS_SELECTOR, 'a:nth-child(2) > div.D_o_ > div > div > img').get_dom_attribute('src')
     toko = i.find_element(By.CSS_SELECTOR, 'a.D_ow.D_iJ > div.D_oz > p').text
     harga = i.find_element(By.CSS_SELECTOR, 'a:nth-child(2) > div.D_oK > p').text
     kondisi = i.find_element(By.CSS_SELECTOR, 'a:nth-child(2) > p:nth-child(4)').text
-    print(image)
+
+    print(judul, harga, image)
+
+    list_barang = {
+        'judul': judul,
+        'image': image,
+        'toko': toko,
+        'harga': harga,
+        'kondisi': kondisi
+    }
+    photography.append(list_barang)
+
+print(photography)
 
 driver.quit()
