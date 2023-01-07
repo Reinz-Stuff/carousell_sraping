2b2fb41ba5c4de2312d914759e8b1f56f94f820b
Fauzi kurniawan <ozilpym123@gmail.com>
2023-01-07 21:14:30 +0700

    selenium download image in element revision

--- downlaod_image/image.py
+++ downlaod_image/image.py
@@ -1,28 +1,49 @@
+import time
 import requests as requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
 driver = webdriver.Chrome()
 driver.get('https://id.carousell.com/categories/photography-6')
 driver.maximize_window()
 
-bottom = False
-a = 0
-while not bottom:
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    driver.execute_script(f"window.scrollTo(0, {a});")
-    if a > new_height:
-        bottom = True
-    a += 5
+wait = WebDriverWait(driver, 3)
 
-data = driver.find_elements(By.CSS_SELECTOR, 'main > div > div > div > div > div.D_uO')
-judul = 1
-for i in data:
-    j = str(judul)
-    image = i.find_element(By.CSS_SELECTOR, 'a:nth-child(2) > div.D_uW > div > div > img').get_dom_attribute('src')
+j = 1
+while True:
+    # Scroll down
+    print(f"scrolling down...{j}")
+    driver.execute_script("window.scrollTo(0, 0);")
+    bottom = False
+    a = 0
+    while not bottom:
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        driver.execute_script(f"window.scrollTo(0, {a});")
+        if a > new_height:
+            bottom = True
+        a += 10
+    driver.execute_script("window.scrollTo(0, 0);")
+    j += 1
 
-    with open('images/' + j + '.jpg', 'wb') as f:
-        img = requests.get(image)
-        f.write(img.content)
-    judul += 1
-driver.quit()
+    try:
+        # Wait for the elements to disappear
+        images = driver.find_elements(By.XPATH,  '//img[@class="D_vo D_vl D_Xz"]')
+        if len(images) == 48:
+            print(f"{len(images)} image found...!")
+            j = 1
+            for image in images:
+                img = image.get_dom_attribute('src')
+                response = requests.get(img)
+                open(f"images/product__{j}.jpg", "wb").write(response.content)
+
+                print(f"image {j} downlaoded!")
+                j += 1
+            break
+        else:
+            print(f"{48 - len(images)} image not loaded...rescrolling")
+            pass
+
+    except:
+        print("no image found")
+        break
