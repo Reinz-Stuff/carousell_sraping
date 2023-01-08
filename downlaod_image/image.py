5a078dbc4c3a6d486f2bd160274be212a6ebc71b
Fauzi kurniawan <ozilpym123@gmail.com>
2023-01-08 09:57:40 +0700

    deleted unused code

--- downlaod_image/image.py
+++ downlaod_image/image.py
@@ -1,8 +1,6 @@
-import time
 import requests as requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 
 driver = webdriver.Chrome()
 driver.get('https://id.carousell.com/categories/photography-6')
