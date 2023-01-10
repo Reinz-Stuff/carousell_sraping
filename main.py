42c8d68ad7e386ecbbb207ad298803edb70c77be
Fauzi kurniawan <ozilpym123@gmail.com>
2023-01-10 13:28:46 +0700

    code simplification

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -39,36 +40,47 @@ def data_extraction():
                                              '//*[@id="main"]/div[2]/div/div[5]/main/div/div/div/div/div[1]/a[2]/div[1]/div/div/img')
                 harga = driver.find_elements(By.XPATH,
                                              '//*[@id="main"]/div[2]/div/div[5]/main/div/div/div/div/div[1]/a[2]/div[2]/p')
+                urls = driver.find_elements(By.XPATH, '//*[@id="main"]/div[2]/div/div[5]/main/div/div/div/div/div[1]/a[2]')
+
                 photography = []
                 j = 1
-                for a, b, c in zip(judul, image, harga):
+                for a, b, c, d in zip(judul, image, harga, urls):
                     title = a.text
                     img = b.get_dom_attribute('src')
                     price = c.text
+                    url = 'https://id.carousell.com' + d.get_dom_attribute('href')
+
                     dicts = {
                         'Nama Produk': title,
                         'Gambar Produk': img,
-                        'Harga Produk': price
+                        'Harga Produk': price,
+                        'link': url
                     }
                     photography.append(dicts)
 
                     print(f"get product detail....{j}")
                     j += 1
 
+                # Making folder
+                try:
+                    os.makedirs('Extraction')
+                except FileExistsError:
+                    print(f'folder already exists.')
+
                 # writing result to files
                 print("writing variable into file...")
                 df = pd.DataFrame(photography)
 
                 # Write the DataFrame to a JSON file
-                df.to_json("result.json", orient="records")
+                df.to_json("Extraction/result.json", orient="records")
                 print(f"JSON file created....!")
 
                 # Write the DataFrame to a CSV file
-                df.to_csv("result.csv", index=False)
+                df.to_csv("Extraction/result.csv", index=False)
                 print(f"CSV file created....!")
 
                 # Write the DataFrame to excell file
-                df.to_excel("result.xlsx", index=False)
+                df.to_excel("Extraction/result.xlsx", index=False)
                 print(f"excel file created....!")
 
                 print("process finished!")
@@ -77,7 +89,7 @@ def data_extraction():
                 print(f"{48 - len(images)} product detail not loaded...rescrolling")
                 pass
 
-        except:
+        except FileExistsError:
             print("cannot find all image")
             break
 
